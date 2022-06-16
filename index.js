0ef3ad5bf7b3d5433823634f9f8eb859734ce3fb
Elna JP <eriosdd@gmail.com>
2022-06-16 08:13:17 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  clickAlert('I was clicked!');
+});
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+input.addEventListener('click', clickAlert);
+
+function addingEventListener() {
+  input.addEventListener('click', function() {
+  clickAlert('I was clicked!');
+});
+}
+input.addingEventListener('click', addingEventListener)
